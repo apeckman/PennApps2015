bf0d46b9d2dbbb9cd52ce1a5c29e9163a7451a80
Ally Sorge <asorge@andrew.cmu.edu>
2015-09-05 13:45:31 -0400

    created initial view

--- bae/stocks/views.py
+++ bae/stocks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Symbol
+
+
+def index(request):
+	price_list = Symbol.objects.all()
+	template = loader.get_template('stocks/index.html')
+	context = RequestContext(request, {
+		'price_list': price_list,
+		})
+	return HttpResponse(template.render(context))
